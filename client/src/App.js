e487a00827b7c72df41deda7072e75fd8dc6e022
alexander <alexander.xin.wang@outlook.com>
2023-01-13 16:13:09 -0500

    Protected Route For Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
 import { Provider } from "react-redux";
@@ -12,31 +14,38 @@ import store from "./store";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
-    useEffect(() => {
-        // check for token in LS when app first runs
-        if (localStorage.token) {
-            // if there is a token set axios headers for all requests
-            setAuthToken(localStorage.token);
-        }
-        // try to fetch a user, if no token or invalid token we
-        // will get a 401 response from our API
-       store.dispatch(loadUser());
-    }, []);
+  useEffect(() => {
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token);
+    }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
+    store.dispatch(loadUser());
+  }, []);
 
-    return (
-        <Provider store={store}>
-            <Router>
-                <Navbar />
-                <Alert />
-                <Route path="/" element={<Landing />} />
-                <Route path="/register" component={Register} />
-                <Route path="/login" component={Login} />
-            </Router>
-        </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route path="/" element={<Landing />} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+              <PrivateRoute path="/dashboard" component={Dashboard} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;

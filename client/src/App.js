fb2fa831980a9f356be8589e25ca61e21cacbc1d
alexander <alexander.xin.wang@outlook.com>
2023-01-14 19:47:08 -0500

    CreateProfile Component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-form/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
@@ -40,6 +41,7 @@ const App = () => {
               <Route path="/register" component={Register} />
               <Route path="/login" component={Login} />
               <PrivateRoute path="/dashboard" component={Dashboard} />
+              <PrivateRoute path="/create-profile" component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
